b66f934d12c7d9130b9a432c9887c4bea2834aaa
alessandrostein <alessandro@alessandrostein.com>
2014-09-01 23:59:26 -0300

    Controller and View Action New 9.19

--- app/controllers/restaurantes_controller.rb
+++ app/controllers/restaurantes_controller.rb
@@ -14,4 +14,18 @@ class RestaurantesController < ApplicationController
 
 		redirect_to(action: "index")
 	end
+
+	def new
+		@restaurante = Restaurante.new
+	end
+
+	def create
+		@restaurante = Restaurante.new(restaurante_params)
+		@restaurante.save
+		redirect_to(action: 'show', id: @restaurante)
+	end
+
+	def restaurante_params
+		params.require(:restaurante).permit(:nome, :endereco, :especialidade)
+	end
 end
